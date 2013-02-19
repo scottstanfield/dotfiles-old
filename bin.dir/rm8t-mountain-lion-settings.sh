8a6f8cc790238bf804ed18c089422137d2565ebe
S. Stanfield <rm8t@me.com>
2013-02-18 22:55:31 -0800

    Don't hide Twitter anymore when it loses focus

--- bin.dir/rm8t-mountain-lion-settings.sh
+++ bin.dir/rm8t-mountain-lion-settings.sh
@@ -401,9 +401,6 @@ defaults write com.twitter.twitter-mac ESCClosesComposeWindow -bool true
 # Show full names rather than Twitter handles
 # defaults write com.twitter.twitter-mac ShowFullNames -bool true
 
-# Hide the app in the background if it’s not the front-most window
-defaults write com.twitter.twitter-mac HideInBackground -bool true
-
 ###############################################################################
 # Personal Additions                                                          #
 ###############################################################################
