3e3f96f28c21ecafecd5076c3631faf40310aae7
Scott Stanfield <scott@vertigo.com>
2014-06-16 17:24:04 -0700

    Trackpad changes for drag, click and scroll

--- bin.dir/osx-defaults.sh
+++ bin.dir/osx-defaults.sh
@@ -63,18 +63,15 @@ defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 # Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
 ###############################################################################
 
-# Trackpad: enable tap to click for this user and for the login screen
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-
-# Trackpad: swipe between pages with three fingers
-# defaults write NSGlobalDomain AppleEnableSwipeNavigateWithScrolls -bool true
-# defaults -currentHost write NSGlobalDomain com.apple.trackpad.threeFingerHorizSwipeGesture -int 1
-# defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 1
-
-# Disable "natural" (Lion-style) scrolling
+# Disables "Scroll direction: natural" in favor of old and correct style.
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+# Tap to click
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+
+# Drag windows with three fingers
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true 
+
 
 # Enable full keyboard access for all controls
 # (e.g. enable Tab in modal dialogs)
